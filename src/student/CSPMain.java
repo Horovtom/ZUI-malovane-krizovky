15d3ea8f03c9c93965435dd23c41a4ca107e4049
Tomáš Hořovský <tomashorovsky@seznam.cz>
2017-03-30 00:11:28 +0200

    Initial commit

---
+++ src/student/CSPMain.java
@@ -0,0 +1,18 @@
+package student;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.logging.Logger;
+
+/**
+ * Created by Hermes235 on 30.3.2017.
+ */
+public class CSPMain {
+    private static final Logger LOGGER = Logger.getLogger(CSPMain.class.getName());
+
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+
+    }
+}
---
+++ src/student/Grid.java
@@ -0,0 +1,20 @@
+package student;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Hermes235 on 30.3.2017.
+ */
+public class Grid {
+    private ArrayList<Integer> colors = new ArrayList<>();
+    private int[][] grid;
+
+    /**
+     * Creates both-way 
+     * @return
+     */
+    public boolean generateBounds() {
+
+    }
+
+}
